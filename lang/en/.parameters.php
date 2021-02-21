eaa6012d1eb74692ef79ec656ca6cd011b4061c6
Ivan <web@logycon.ru>
2021-02-22 00:33:28 +0300

    #1 hint added

--- lang/en/.parameters.php
+++ lang/en/.parameters.php
@@ -1,3 +1,4 @@
-<?
-$MESS["SMSN_SOME_TEXT"] = "Nothing";
-?>
+<?php
+$MESS['SMSN_FEEDBACK_ERROR_ENABLED_PARAM_CAPTION'] = 'Show a hint for a user';
+$MESS['SMSN_FEEDBACK_ERROR_PLACEHOLDER_PARAM_CAPTION'] = 'A hint for a user';
+$MESS['SMSN_FEEDBACK_ERROR_HINT'] = 'If you find an error in the text, please select a fragment of the text with it and press Ctrl + Enter';
--- lang/ru/.parameters.php
+++ lang/ru/.parameters.php
@@ -1,2 +1,4 @@
 <?php
-$MESS["SMSN_SOME_TEXT"] = "Nothing";
+$MESS['SMSN_FEEDBACK_ERROR_ENABLED_PARAM_CAPTION'] = 'Показывать подсказку для пользователя';
+$MESS['SMSN_FEEDBACK_ERROR_PLACEHOLDER_PARAM_CAPTION'] = 'Подсказка для пользователя';
+$MESS['SMSN_FEEDBACK_ERROR_HINT'] = 'Если нашли в тексте ошибку, просьба выделить фрагмент текста с ней и нажать Ctrl+Enter';
